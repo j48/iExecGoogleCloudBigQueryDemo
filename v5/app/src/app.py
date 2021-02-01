f7da57055b5bb70ca91cb89d17bb312d65e78f5e
j48 <52556841+j48@users.noreply.github.com>
2021-02-01 01:32:57 -0600

    Update app.py

--- v5/app/src/app.py
+++ v5/app/src/app.py
@@ -10,15 +10,16 @@ from eth_abi import encode_single
 from eth_utils import to_hex
 from google.cloud import bigquery
 
+iexec_root = '/'
+iexec_in = getenv('IEXEC_IN') or f'{iexec_root}iexec_in'
+iexec_out = getenv('IEXEC_OUT') or f'{iexec_root}iexec_out'
+dataset = getenv("IEXEC_DATASET_FILENAME") or 'bigquery.json'
+dataset_loc = f'{iexec_in}/{dataset}'
 
-iexec_in = getenv('IEXEC_IN') or './iexec_in'
-iexec_out = getenv('IEXEC_OUT') or './iexec_out'
-deterministic = 'result.txt'  # file used for deterministic comparison
-error_callback = True  # handle errors with callback or log.txt
+deterministic_file = 'result.txt'  # file used for deterministic comparison
 
 default_coins = ['BTC', 'ETH']  # will always be in query
 max_input = 10  # default number of user input/coins (does not include default)
-credentials = 'credentials.json'  # can set as env
 
 
 class ErrorCallback(Exception):
@@ -27,7 +28,8 @@ class ErrorCallback(Exception):
         2: "credentials file not found or corrupted",
         3: "general bigquery query error",
         4: "error creating csv",
-        5: "auto callback test error"
+        5: "auto callback test error",
+        6: "general dapp error"
     }
 
     def __init__(self, code):
@@ -40,15 +42,10 @@ class ErrorCallback(Exception):
 
 def stdout(txt):
     # used for TEE log since regular stdout is not saved (use instead of print)
+    # adding try for sanity test (will try iexec_out first)
     with open(f'{iexec_out}/log.txt', 'a') as f:
         with redirect_stdout(f):
             print(f'{txt}')
-
-
-def add_callback(cb):
-    # https://docs.soliditylang.org/en/v0.5.3/abi-spec.html
-    with open(f'{iexec_out}/computed.json', 'w+') as f:
-        json_dump({'callback-data': cb}, f, indent=2)
 
 
 def create_receipt(txt):
@@ -57,20 +54,27 @@ def create_receipt(txt):
         f.write(txt)
 
 
+def create_error_file():
+    # create an error file so results can be filtered
+    # I guess no csv would indicate an error too
+    with open(f'{iexec_out}/ERROR.txt', 'w+') as f:
+        f.write('')
+
+
 def create_deterministic(txt):
     # text string converted to sha256 hash
     # generating the deterministic file is user preference but must be consistent
     sha = sha256(str(txt).encode('utf-8')).hexdigest()
+    deterministic_loc = f'{iexec_out}/{deterministic_file}'
 
-    with open(f'{iexec_out}/{deterministic}', 'w+') as f:
+    with open(deterministic_loc, 'w+') as f:
         f.write(sha)
 
-    with open(f'{iexec_out}/computed.json', 'w+') as f:
-        json_dump({'deterministic-output-path': f'{iexec_out}/{deterministic}'}, f, indent=2)
+    return deterministic_loc
 
 
 def create_csv(rows):
-    cvs_data = {'header': ['coin', 'price', 'cap', 'date'], 'coins': {}}
+    cvs_data = {"header": ["coin", "price", "cap", "date"], "coins": {}}
 
     for row in rows:
         # can filter results here or in JS
@@ -95,8 +99,11 @@ def create_csv(rows):
             for coin in cvs_data['coins']:
                 for coin_date in cvs_data['coins'][coin]:
                     writer.writerow([coin, coin_date] + cvs_data['coins'][coin][coin_date])
+
     except Exception:
-        raise ErrorCallback(4)
+        return False
+
+    return True
 
 
 def analyze_user_input(argv):
@@ -113,7 +120,7 @@ def analyze_user_input(argv):
 
     # for testing purposes
     if 'E5CB' in dapp_input:
-        raise ErrorCallback(5)
+        return 5
 
     for arg in dapp_input:
         if arg.isalnum():
@@ -126,14 +133,14 @@ def analyze_user_input(argv):
     return sorted(list(set(dapp_input_valid)))
 
 
-def get_dataset_table():
+def get_dataset_table(loc):
     # can have table hardcoded or in dataset (should dataset name be a secret?)
     try:
-        with open(f'{iexec_in}/bigquery.json', 'r+') as f:
+        with open(loc, 'r') as f:
             data = json_load(f)
             return data['dataset']
     except Exception:
-        raise ErrorCallback(1)
+        return False
 
 
 def create_error_callback(error_code):
@@ -147,21 +154,37 @@ def create_error_callback(error_code):
     return to_hex(error_bytes)
 
 
+def create_computed_json(dic):
+    with open(f'{iexec_out}/computed.json', 'w+') as f:
+        json_dump(dic, f, indent=2)
+
+
 if __name__ == '__main__':
+    computed = {}
+    callback = None
+    deterministic = None
+
     stdout(f'Start: {str(time())}')
     stdout(f'Max Input: {max_input}')
     stdout(f'Default Coins: {default_coins}')
     stdout(f'Input: {sys_argv}')
 
-    try:
-        user_input_valid = analyze_user_input(sys_argv)
+    user_input_valid = analyze_user_input(sys_argv)
 
-        stdout(f'Valid Input: {user_input_valid}')
-        stdout(f'running dapp...')
+    stdout(f'Valid Input: {user_input_valid}')
+    stdout(f'running dapp...')
+
+    try:
+        if user_input_valid == 5:
+            raise ErrorCallback(5)
 
         coins = sorted(list(set(user_input_valid + default_coins)))
         coins_sql = ', '.join(f'"{c}"' for c in coins)
-        table = get_dataset_table()
+
+        table = get_dataset_table(dataset_loc)
+        if not table:
+            raise ErrorCallback(1)
+
         sql = (
             f'SELECT coin, price, cap, date '
             f'FROM `{table}` '
@@ -171,51 +194,70 @@ if __name__ == '__main__':
         )
 
         try:
-            if credentials:
-                client = bigquery.Client.from_service_account_json(f'{iexec_in}/{credentials}')
-            else:
-                client = bigquery.Client()
-
+            # can set json location as env
+            client = bigquery.Client.from_service_account_json(dataset_loc)
             q = client.query(sql)
             stdout(f'querying API...')
             q.result()  # this is where API is queried
-
         except FileNotFoundError:
             raise ErrorCallback(2)
-        except Exception:
+        except Exception as e:
             raise ErrorCallback(3)
-        else:
-            results = list(q)
-            stdout(f'results received...')
-            create_csv(results)
+
+        results = list(q)
+        stdout(f'results received...')
+
+        csv_success = create_csv(results)
+        if csv_success:
             stdout(f'data.csv created...')
+        else:
+            raise ErrorCallback(4)
 
-            create_receipt(
-                f'Google Cloud - Big Query Receipt\n'
-                f'Job ID: {q.job_id}\n'
-                f'Job Type: {q.job_type}\n'
-                f'Created: {q.created}\n'
-                f'Location: {q.location}\n'
-                f'Project: {q.project}\n'
-                f'Query: {q.query}\n'
-                f'Results: {len(results)}\n'
-                f'Bytes Processed: {q.total_bytes_processed}\n'
-                f'Bytes Billed: {q.total_bytes_billed}\n'
-                f'Ended: {q.ended}\n'
-                f'ETag: {q.etag}'
-            )
-            stdout(f'receipt.txt created...')
+        create_receipt(
+            f'Google Cloud - Big Query Receipt\n'
+            f'Created: {q.created}\n'
+            f'Job ID: {q.job_id}\n'
+            f'Job Type: {q.job_type}\n'
+            f'Location: {q.location}\n'
+            f'Project: {q.project}\n'
+            f'Query: {q.query}\n'
+            f'Results: {len(results)}\n'
+            f'Bytes Processed: {q.total_bytes_processed}\n'
+            f'Bytes Billed: {q.total_bytes_billed}\n'
+            f'ETag: {q.etag}\n'
+            f'Ended: {q.ended}'
+        )
+        stdout(f'receipt.txt created...')
 
-            # can use unique per task or generic
-            create_deterministic(str(q.query))
-            stdout(f'deterministic file created...')
+        # can use unique per task or generic
+        deterministic = create_deterministic(str(q.query))
+        stdout(f'deterministic file created...')
 
     except ErrorCallback as e_callback:
-        stdout(f'*****ERROR******: ({e_callback.code}) {e_callback.msg}')
-        if error_callback:
-            callback = create_error_callback(e_callback.code)
-            add_callback(callback)
-        else:
-            create_deterministic("ERROR")
+        # callback = create_error_callback(e_callback.code)
+        stdout(f'ERROR: ({e_callback.code}) {e_callback.msg}')
+
+    except Exception:
+        # callback = create_error_callback(6)
+        stdout(f'ERROR: (6) general dapp error')
+
+    # without a deterministic the worker freezes
+    # cant combo deterministic with callback error or replace...
+    # must always upload to IPFS on error?
+    # leave for to-do list
+    if deterministic:
+        computed["deterministic-output-path"] = deterministic
+    else:
+        deterministic = create_deterministic('error')
+        stdout(f'deterministic error file created...')
+        computed["deterministic-output-path"] = deterministic
+        create_error_file()
+        stdout(f'error file created...')
+
+    if callback:
+        #  computed["callback-data"] = callback
+        pass
+
+    create_computed_json(computed)
 
     stdout(f'Done: {str(time())}')
