a1a25f935f89900825f20c90d7f7a4b5b8d8ad6a
j48 <52556841+j48@users.noreply.github.com>
2021-02-19 09:52:38 -0600

    Update app.py

--- v5/app/src/app.py
+++ v5/app/src/app.py
@@ -6,14 +6,12 @@ from os import getenv
 from sys import argv as sys_argv
 from time import time
 
-from eth_abi import encode_single
-from eth_utils import to_hex
 from google.cloud import bigquery
 
 iexec_root = '/'
 iexec_in = getenv('IEXEC_IN') or f'{iexec_root}iexec_in'
 iexec_out = getenv('IEXEC_OUT') or f'{iexec_root}iexec_out'
-dataset = getenv("IEXEC_DATASET_FILENAME") or 'bigquery.json'
+dataset = 'bigquery.json'
 dataset_loc = f'{iexec_in}/{dataset}'
 
 deterministic_file = 'result.txt'  # file used for deterministic comparison
@@ -40,46 +38,55 @@ class ErrorCallback(Exception):
         return repr(self.msg)
 
 
-def stdout(txt):
+def stdout(t):
     # used for TEE log since regular stdout is not saved (use instead of print)
     # adding try for sanity test (will try iexec_out first)
     with open(f'{iexec_out}/log.txt', 'a') as f:
         with redirect_stdout(f):
-            print(f'{txt}')
+            print(f'{t}')
 
 
-def create_receipt(txt):
+def create_receipt(t):
     # optional to generate receipt for records
     with open(f'{iexec_out}/receipt.txt', 'w+') as f:
-        f.write(txt)
+        f.write(t)
 
 
-def create_error_file():
+def create_error_file(e):
     # create an error file so results can be filtered
     # I guess no csv would indicate an error too
     with open(f'{iexec_out}/ERROR.txt', 'w+') as f:
-        f.write('')
+        f.write(e)
 
 
-def create_deterministic(txt):
+def create_computed_json(c):
+    with open(f'{iexec_out}/computed.json', 'w+') as f:
+        json_dump(c, f, indent=2)
+
+
+def create_deterministic(t):
     # text string converted to sha256 hash
     # generating the deterministic file is user preference but must be consistent
-    sha = sha256(str(txt).encode('utf-8')).hexdigest()
+    sha = sha256(str(t).encode('utf-8')).hexdigest()
     deterministic_loc = f'{iexec_out}/{deterministic_file}'
 
     with open(deterministic_loc, 'w+') as f:
         f.write(sha)
 
-    return deterministic_loc
+    computed = {"deterministic-output-path": deterministic_loc}
+
+    create_computed_json(computed)
+
+    return True
 
 
-def create_csv(rows):
+def create_csv(r):
     cvs_data = {"header": ["coin", "price", "cap", "date"], "coins": {}}
 
-    for row in rows:
+    for row in r:
         # can filter results here or in JS
         if row.cap != 0:
-            date_key = row.date.strftime('%m-%d-%Y')
+            date_key = row.date.strftime('%Y-%m-%d')
             # using date as key to filter out possible dupes
             try:
                 # optional format here so it's easier to read in csv
@@ -98,15 +105,17 @@ def create_csv(rows):
 
             for coin in cvs_data['coins']:
                 for coin_date in cvs_data['coins'][coin]:
-                    writer.writerow([coin, coin_date] + cvs_data['coins'][coin][coin_date])
-
+                    writer.writerow([coin,
+                                     cvs_data['coins'][coin][coin_date][0],
+                                     cvs_data['coins'][coin][coin_date][1],
+                                     coin_date])
     except Exception:
         return False
 
     return True
 
 
-def analyze_user_input(argv):
+def analyze_user_input(a):
     # takes user input then formats in a way that's readable for dapp logic
     # main purpose is to prevent exploits and user error
 
@@ -114,7 +123,7 @@ def analyze_user_input(argv):
     input_count = 0
 
     try:
-        dapp_input = argv[1:]
+        dapp_input = a[1:]
     except IndexError:
         dapp_input = None
 
@@ -133,36 +142,18 @@ def analyze_user_input(argv):
     return sorted(list(set(dapp_input_valid)))
 
 
-def get_dataset_table(loc):
+def get_dataset_table(j):
     # can have table hardcoded or in dataset (should dataset name be a secret?)
     try:
-        with open(loc, 'r') as f:
+        with open(j, 'r') as f:
             data = json_load(f)
             return data['dataset']
     except Exception:
         return False
 
 
-def create_error_callback(error_code):
-    # easier error logging of TEE dapp to avoid IPFS upload
-    # error should be in a format easily determined by smart contract/web3
-    # https://eth-abi.readthedocs.io/en/latest/encoding.html
-    # The value is stored in the resultsCallback field of the Task object in the IexecProxy smart contract.
-    error_args = error_code
-    error_bytes = encode_single('uint8', error_args)
-
-    return to_hex(error_bytes)
-
-
-def create_computed_json(dic):
-    with open(f'{iexec_out}/computed.json', 'w+') as f:
-        json_dump(dic, f, indent=2)
-
-
 if __name__ == '__main__':
-    computed = {}
-    callback = None
-    deterministic = None
+    error = None
 
     stdout(f'Start: {str(time())}')
     stdout(f'Max Input: {max_input}')
@@ -201,7 +192,7 @@ if __name__ == '__main__':
             q.result()  # this is where API is queried
         except FileNotFoundError:
             raise ErrorCallback(2)
-        except Exception as e:
+        except Exception:
             raise ErrorCallback(3)
 
         results = list(q)
@@ -230,34 +221,27 @@ if __name__ == '__main__':
         stdout(f'receipt.txt created...')
 
         # can use unique per task or generic
-        deterministic = create_deterministic(str(q.query))
+        create_deterministic(str(q.query))
         stdout(f'deterministic file created...')
 
     except ErrorCallback as e_callback:
-        # callback = create_error_callback(e_callback.code)
-        stdout(f'ERROR: ({e_callback.code}) {e_callback.msg}')
+        stdout(f'**ERROR**')
+        error = f'{e_callback.code}) {e_callback.msg}'
 
     except Exception:
-        # callback = create_error_callback(6)
-        stdout(f'ERROR: (6) general dapp error')
+        stdout(f'**ERROR**')
+        error = 'ERROR: (6) general dapp error'
 
     # without a deterministic the worker freezes
     # cant combo deterministic with callback error or replace...
     # must always upload to IPFS on error?
-    # leave for to-do list
-    if deterministic:
-        computed["deterministic-output-path"] = deterministic
-    else:
+
+    if error:
         deterministic = create_deterministic('error')
         stdout(f'deterministic error file created...')
-        computed["deterministic-output-path"] = deterministic
-        create_error_file()
+
+        # error file for easier filtering
+        create_error_file(error)
         stdout(f'error file created...')
-
-    if callback:
-        #  computed["callback-data"] = callback
-        pass
-
-    create_computed_json(computed)
 
     stdout(f'Done: {str(time())}')
